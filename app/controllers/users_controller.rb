8bd2fb9d72198d5e21a0e751a9fa0306240ad7db
Tatsuya <tatsutatsuyan0529@gmail.com>
2020-02-12 05:12:18 +0000

    finish editing-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  
+  def index
+    @users = User.all
+    @users = User.search(params[:search]).paginate(page: params[:page], per_page: 20)
+  end
   
   def show
-    @user = User.find(params[:id])
   end
   
   
@@ -20,9 +28,56 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザーの更新に成功しました。"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+  
+  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    
+    # beforeフィルター
+    
+    # paramsハッシュからユーザーを取得する
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    #ログイン済みのユーザーかを検証
+    
+    def logged_in_user
+      unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+  
+    # アクセスしたユーザーが現在ログインしているユーザーかを確認する
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # システム管理権限所有かを判定
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # remember_tokenと言う仮想の属性を作成する
+  attr_accessor :remember_token
   #selfはユーザーオブジェクトを指している
   before_save { self.email = email.downcase }
   
@@ -8,5 +10,47 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続セッションのためハッシュ化したトークンをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # トークンがダイジェストと一致すればtrueを返す
+  def authenticated?(remember_token)
+    # ダイジェストが存在しない場合はfalseを返して終了
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end 
+  
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
+  def self.search(search)
+    if search
+      where(['name LIKE ?', "%#{search}%"]) # 検索とusernameの部分一致を表示
+    else
+      all
+    end
+  end
 end
