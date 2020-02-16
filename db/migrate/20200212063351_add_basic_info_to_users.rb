1032875dda9a61b4a2cacc01bfcaa1739eaaccf5
Tatsuya <tatsutatsuyan0529@gmail.com>
2020-02-16 07:22:26 +0000

    finish show

--- db/migrate/20200212063351_add_basic_info_to_users.rb
+++ db/migrate/20200212063351_add_basic_info_to_users.rb
@@ -2,5 +2,7 @@ class AddBasicInfoToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :basic_time, :datetime, default: Time.current.change(hour: 8, min: 0, sec: 0)
     add_column :users, :work_time, :datetime, default: Time.current.change(hour: 7, min: 30, sec: 0)
+    add_column :users, :work_start_time, :datetime, default: Time.current.change(hour: 9, min: 0, sec: 0)
+    add_column :users, :work_finish_time, :datetime, default: Time.current.change(hour: 18, min: 0, sec: 0)
   end
 end
